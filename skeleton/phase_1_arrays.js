eed59e37a30e901fa69635716f25b290ddb62d5e
kcho760 <kcho760@gmail.com>
2023-04-25 13:45:52 -0400

    twosum

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -9,6 +9,20 @@ Array.prototype.myUniq = function () {
 
     // console.log(newArr);
     return newArr;
-}
+};
 
 [1, 1, 2, 2, 3, 3].myUniq();
+
+Array.prototype.twoSum = function () {
+    const pairs = []
+
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i+1; j < this.length; j++) {
+            if (this[i] + this[j] === 0) pairs.push([i,j]);
+        }
+    }
+
+    console.log(pairs);
+};
+
+[-2,-3,-1,2,3,4,1].twoSum();
